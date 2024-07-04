b4b45e1535ba7e1b11d0811ea299cc52606c1316
SurajMotwani1306 <suraj.motwani1306@gmail.com>
2024-07-04 02:18:47 -0700

    Basic code added for dropdown select

--- projects/angular-dropdown-select/src/lib/angular-dropdown-select.component.ts
+++ projects/angular-dropdown-select/src/lib/angular-dropdown-select.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-angular-dropdown-select',
-  template: `
-    <p>
-      angular-dropdown-select works!
-    </p>
-  `,
-  styles: ``
+  templateUrl:'./angular-dropdown-select.component.html',
+  styleUrl: './angular-dropdown-select.component.css'
 })
 export class AngularDropdownSelectComponent {
 
+  @Input() headings: any;
+  dropdownOpen = false;
+  items: any;
+
+  constructor(){}
+
+  ngOnInit(){
+   this.items = this.headings;
+  }
+
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  onSelectAll(event: Event): void {
+    const checkbox = event.target as HTMLInputElement;
+    this.items.forEach((item:any) => item.checked = checkbox.checked);
+  }
+
+  onItemSelected(item: any): void {
+    item.checked = !item.checked;
+  }
 }
--- projects/angular-dropdown-select/src/lib/angular-dropdown-select.module.ts
+++ projects/angular-dropdown-select/src/lib/angular-dropdown-select.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AngularDropdownSelectComponent } from './angular-dropdown-select.component';
-
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AngularDropdownSelectComponent
   ],
   imports: [
+    BrowserModule,
+    FormsModule
   ],
   exports: [
     AngularDropdownSelectComponent
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AngularDropdownSelectModule { }
